ec8af1dca0f8fa7f63b91b09d1098375b0217b3e
xuxl90 <xuelixu90@gmail.com>
2026-09-01 19:54:28 +0800

    Delete SerumSourceAnalyzer directory

--- SerumSourceAnalyzer/R/serumsource-package.R
+++
@@ -1,9 +0,0 @@
-#' @keywords internal
-"_PACKAGE"
-
-## usethis namespace: start
-#' @importFrom magrittr %>%
-#' @importFrom stats median sd mad pnorm setNames
-#' @importFrom utils write.csv read.csv
-## usethis namespace: end
-NULL
--- SerumSourceAnalyzer/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(SerumSourceAnalyzer)
-
-test_check("SerumSourceAnalyzer")
